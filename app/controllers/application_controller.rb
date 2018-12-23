53d696bf202a4353294702c5768b33cb97c7de43
Celso Silva <silva.celso29@gmail.com>
2018-12-23 13:50:55 -0500

    end of line error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [:email, :password, :password_confirmation, :remember_me, :avatar, :avatar, :avatar_cache]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-
+  end 
 end
