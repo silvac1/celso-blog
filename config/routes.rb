fc4f0d25a8aa1457712560a2a433694546a4a083
Celso Silva <silva.celso29@gmail.com>
2019-03-27 23:57:56 -0400

    style changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#welcome'
+
   devise_for :admins
   mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
 
   resources :posts
 end
